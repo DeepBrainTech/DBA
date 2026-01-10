8b4c4172595a32207baa8141901325dc7fbee4db
wjm <114782942+jingmaowang@users.noreply.github.com>
2026-01-10 14:04:44 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -253,8 +253,8 @@ const Hero = () => (
             </p>
             <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
               <div className="rounded-md shadow">
-                <a href="#" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg">
-                  Book Assessment
+                <a href= "https://docs.google.com/forms/d/e/1FAIpQLSfAceUQq9JJ3uu-EcWlHkTQ1O1jY_uzh-fptrN7_JC9rf-dqg/viewform?usp=header" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg">
+                  Free Trial Class
                 </a>
               </div>
               <div className="mt-3 sm:mt-0 sm:ml-3">
